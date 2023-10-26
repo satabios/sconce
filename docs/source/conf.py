e86d2b82706db567c549879955e470318cf29b44
satabios <max.satabiossathya@gmail.com>
2023-10-25 21:47:14 -0700

    updated conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -30,6 +30,11 @@ intersphinx_disabled_domains = ['std']
 
 templates_path = ['_templates']
 
+# source_suffix = ['.rst', '.md']
+source_suffix = ".rst"
+
+# The master toctree document.
+master_doc = "index"
 # -- Options for HTML output
 
 html_theme = 'sphinx_rtd_theme'
