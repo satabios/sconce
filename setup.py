6596a252fd9d2b42ec6946362480ce35e8e97f0f
satabios <max.satabiossathya@gmail.com>
2023-11-05 12:42:10 -0800

    updated readme

--- setup.py
+++ setup.py
@@ -5,9 +5,6 @@ from setuptools import setup, find_packages
 with open("README.rst", encoding="utf-8") as readme_file:
     readme = readme_file.read()
 
-# with open("HISTORY.rst") as history_file:
-#     history = history_file.read()
-
 # fmt: off
 __version__ = '0.57'
 # fmt: on
