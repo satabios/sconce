eeb2353688171ebeedb83ff9af21282b10e14207
satabios <max.satabiossathya@gmail.com>
2023-11-05 11:48:43 -0800

    updated docs

---
+++ build-docs-push.sh
@@ -0,0 +1,22 @@
+#! /bin/bash
+
+
+
+git_push() {
+
+git add .
+git commit -m "$1"
+git push -u origin main
+}
+
+
+cd docs/
+make html
+cd ../
+
+# echo "Message to Push?"
+# read message
+# git_push "$message"
+
+
+
