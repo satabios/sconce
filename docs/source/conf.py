99bb6984138a3141903f5711a80f29cda66700e2
satabios <max.satabiossathya@gmail.com>
2023-11-05 13:13:22 -0800

    updated docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -9,11 +9,11 @@ project = 'sconce'
 copyright = '2023, Sathyaprakash'
 author = 'Sathyaprakash'
 
-release = '0.1'
-version = '0.1.0'
+release = '0.57'
+version = '0.57.0'
 
 # -- General configuration
-
+html_logo = "https://github.com/satabios/sconce/blob/master/docs/source/images/sconce-punch-bk_removed.png?raw=true"
 
 extensions = [
     'sphinx.ext.duration',
@@ -38,19 +38,5 @@ html_theme = 'sphinx_rtd_theme'
 
 # -- Options for EPUB output
 epub_show_urls = 'footnote'
-html_logo = "https://github.com/satabios/sconce/blob/master/docs/source/images/sconce-punch-bk_removed.png?raw=true"
 
-# intersphinx_mapping = {
-#     'python': ('https://docs.python.org/3/', None),
-#     'sphinx': ('https://www.sphinx-doc.org/en/master/', None),
-# }
-# intersphinx_disabled_domains = ['std']
 
-# templates_path = ['_templates']
-
-# # -- Options for HTML output
-
-# html_theme = 'sphinx_rtd_theme'
-
-# # -- Options for EPUB output
-# epub_show_urls = 'footnote'
