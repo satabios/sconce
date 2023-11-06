cba707a9dd41598174514e6535411487e0946310
satabios <max.satabiossathya@gmail.com>
2023-11-06 15:43:02 -0800

    removed data

--- build-docs-package-push.sh
+++ build-docs-package-push.sh
@@ -13,18 +13,15 @@ rm -rf build/
 rm -rf .eggs/
 
 
-
 echo "Version-To-Update?"
 read value
 version="version = \"${value}\""
-sed -i "3s/.*/$version/" pyproject.toml
+sed -i "7s/.*/$version/" pyproject.toml
 
 
 cd docs/
 make html
 cd ../
-
-
 
 python3 setup.py clean --all sdist bdist_wheel
 twine upload dist/* --verbose
