e83ad514e2c4135da9530747ba9cc2d261633308
satabios <max.satabiossathya@gmail.com>
2023-11-06 16:11:27 -0800

    updated tests

--- build-docs-package-push.sh
+++ build-docs-package-push.sh
@@ -7,25 +7,30 @@ git commit -m "$1"
 git push -u origin main
 }
 
-
+#Remove all build files
 rm -rf dist/
 rm -rf build/
 rm -rf .eggs/
 
+#Run Tests
+flake8 sconce test
+black sconce test
 
 echo "Version-To-Update?"
 read value
 version="version = \"${value}\""
 sed -i "7s/.*/$version/" pyproject.toml
 
-
+#Build Docs
 cd docs/
 make html
 cd ../
 
+#Build Package
 python3 setup.py clean --all sdist bdist_wheel
 twine upload dist/* --verbose
 
+#Push to GitHub
 echo "Message to Push?"
 read message
 git_push "$message"
--- build/lib/sconce/__init__.py
+++ build/lib/sconce/__init__.py
@@ -1,3 +1,3 @@
 from .sconce import sconce
 
-__all__ = [ "sconce"]
+__all__ = ["sconce"]
