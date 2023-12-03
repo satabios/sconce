10c8c37d1433965cd4ed142864564ae9c195940d
satabios <max.satabiossathya@gmail.com>
2023-12-02 21:49:17 -0800

    updated conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -4,9 +4,7 @@
 # -- Project information
 import os
 import sys
-import sphinx_rtd_theme
-
-
+sys.path.insert(0, os.path.abspath('../../sconce'))
 project = 'sconce'
 copyright = '2023, Sathyaprakash'
 author = 'Sathyaprakash'
@@ -14,22 +12,18 @@ author = 'Sathyaprakash'
 release = '0.57'
 version = '0.57.0'
 
-
-sys.path.insert(0, os.path.abspath(".."))
 # -- General configuration
 html_logo = "https://github.com/satabios/sconce/blob/master/docs/source/images/sconce-punch-bk_removed.png?raw=true"
 
-# extensions = [
-#     'sphinx_rtd_theme',
-#     'sphinx.ext.duration',
-#     'sphinx.ext.doctest',
-#     'sphinx.ext.autodoc',
-#     'sphinx.ext.autosummary',
-#     'sphinx.ext.intersphinx',
-# ]
-extensions = ["sphinx.ext.autodoc", "sphinx.ext.viewcode"]
+extensions = [
+    'sphinx.ext.duration',
+    'sphinx.ext.doctest',
+    'sphinx.ext.autodoc',
+    'sphinx.ext.autosummary',
+    'sphinx.ext.intersphinx',
+]
 html_logo = "images/sconce-punch-bk_removed.png"
-pygments_style = "sphinx"
+
 intersphinx_mapping = {
     'python': ('https://docs.python.org/3/', None),
     'sphinx': ('https://www.sphinx-doc.org/en/master/', None),
@@ -40,7 +34,7 @@ templates_path = ['_templates']
 
 # -- Options for HTML output
 
-html_theme = "sphinx_rtd_theme"
+html_theme = 'sphinx_rtd_theme'
 
 # -- Options for EPUB output
 epub_show_urls = 'footnote'
