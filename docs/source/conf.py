7e70043fd877b9fdae92b4660007cc0964c972fc
satabios <max.satabiossathya@gmail.com>
2023-12-02 22:00:05 -0800

    updated static

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -37,6 +37,7 @@ templates_path = ['_templates']
 # -- Options for HTML output
 
 html_theme = 'sphinx_rtd_theme'
+html_static_path = ["_static"]
 
 # -- Options for EPUB output
 epub_show_urls = 'footnote'
