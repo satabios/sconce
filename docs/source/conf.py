d5bcf23387c8cb458ebe15e5454b5fbf5862e722
satabios <max.satabiossathya@gmail.com>
2023-12-02 21:34:28 -0800

    updated conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -23,6 +23,7 @@ extensions = [
     'sphinx.ext.autodoc',
     'sphinx.ext.autosummary',
     'sphinx.ext.intersphinx',
+    'sphinx_rtd_theme'
 ]
 html_logo = "images/sconce-punch-bk_removed.png"
 
