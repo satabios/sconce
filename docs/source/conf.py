38a10748b77bdf68876968f5183c30e495a8da3d
satabios <max.satabiossathya@gmail.com>
2023-12-02 22:01:29 -0800

    updated html

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -13,7 +13,7 @@ release = '0.57'
 version = '0.57.0'
 
 
-sys.path.insert(0, os.path.abspath(".."))
+sys.path.insert(0, os.path.abspath("../../"))
 # -- General configuration
 html_logo = "https://github.com/satabios/sconce/blob/master/docs/source/images/sconce-punch-bk_removed.png?raw=true"
 
