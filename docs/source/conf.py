c674dad3d713e83cb67b0a6af376787966f4f57d
Sathyaprakash Narayanan <max.satabiossathya@gmail.com>
2023-12-05 13:19:33 -0800

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -5,7 +5,7 @@
 import os
 import sys
 
-sys.path.insert(0, os.path.abspath(os.path.join('..', '..', 'sconce')))
+sys.path.insert(0, os.path.abspath(os.path.join('sconce/')))
 print("PATH Direc:",os.getcwd())
 print("LS:",os.listdir('./'))
 # -- Project information -----------------------------------------------------
