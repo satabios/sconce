ce3cd6e21990b40295cd19163cfd6d6e3dac9691
satabios <max.satabiossathya@gmail.com>
2023-12-29 20:11:45 -0800

    fixed conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -8,13 +8,6 @@ import sys
 
 sys.path.insert(0, os.path.abspath(os.path.join("..", "..", "sconce")))
 
-
-try:
-  import sconce
-except ImportError:
-  print("oh no 1")
-
-
 # sys.path.insert(0, os.path.abspath(os.path.join('../../sconce/')))
 print("PATH Direc:",os.getcwd())
 print("LS:",os.listdir('./'))
