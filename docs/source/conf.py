d39575a4f74ab02fcb7821b5d0e70267b7019884
satabios <max.satabiossathya@gmail.com>
2023-12-30 21:29:30 -0800

    updated quant rst and conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -6,6 +6,7 @@ import os
 import sys
 
 sys.path.insert(0, os.path.abspath(os.path.join('../../sconce')))
+print(sys.path)
 print("PATH Direc:",os.getcwd())
 print("LS:",os.listdir('./'))
 print("LSD:",os.listdir('../../sconce/'))
