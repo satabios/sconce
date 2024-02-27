2caff8e8ef309fec562ada61efbed4310354e1e3
Sathyaprakash Narayanan <max.satabiossathya@gmail.com>
2024-02-27 09:00:24 -0800

    updated readme with sconce roadmap

--- build-docs-push.sh
+++ build-docs-push.sh
@@ -12,10 +12,10 @@ git push -u origin main
 
 
 #Generate rst files
-tutorials_folder_path="tutorials"
-for file in "$tutorials_folder_path"/*.ipynb; do
-  jupyter nbconvert --to rst --output-dir="docs/source/tutorials/" "$file"
-done
+# tutorials_folder_path="tutorials"
+# for file in "$tutorials_folder_path"/*.ipynb; do
+#   jupyter nbconvert --to rst --output-dir="docs/source/tutorials/" "$file"
+# done
 
 
 
