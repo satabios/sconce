b15b6552dc29c3fefeaa696e8d68125278bb819b
PrAsAnNaRePo <prasannatwenty@gmail.com>
2024-05-20 13:15:55 +0530

    Created performance_eval class along with sconces.

---
+++ sconce/preformance_eval.py
@@ -0,0 +1,275 @@
+import os
+
+import snntorch
+from snntorch import functional as SF
+from snntorch import utils
+
+import torch
+from torch import nn
+
+import time
+from prettytable import PrettyTable
+from tqdm import tqdm
+
+class PerformanceEval:
+    def __init__(self, dataloader, snn, model, snn_num_steps) -> None:
+        self.dataloader = dataloader
+        self.snn = snn
+        self.model = model
+        self.snn_num_steps = snn_num_steps
+
+    def load_torchscript_model(self, model_filepath, device):
+        model = torch.jit.load(model_filepath, map_location=device)
+        return model
+
+    def measure_inference_latency(
+			self, model, device, input_data, num_samples=100, num_warmups=10
+	):
+        model.to(device)
+        model.eval()
+        
+        x = input_data.to(device)
+        
+        with torch.no_grad():
+            for _ in range(num_warmups):
+                _ = model(x)
+        torch.cuda.synchronize()
+        
+        with torch.no_grad():
+            start_time = time.time()
+            for _ in range(num_samples):
+                _ = model(x)
+                torch.cuda.synchronize()
+            end_time = time.time()
+        elapsed_time = end_time - start_time
+        elapsed_time_ave = elapsed_time / num_samples
+        
+        return elapsed_time_ave
+
+    @torch.no_grad()
+    def measure_latency(self, model, dummy_input, n_warmup=20, n_test=100):
+        """
+		Measures the average latency of a given PyTorch model by running it on a dummy input multiple times.
+
+		Args:
+		  model (nn.Module): The PyTorch model to measure the latency of.
+		  dummy_input (torch.Tensor): A dummy input to the model.
+		  n_warmup (int, optional): The number of warmup iterations to run before measuring the latency. Defaults to 20.
+		  n_test (int, optional): The number of iterations to run to measure the latency. Defaults to 100.
+
+		Returns:
+		  float: The average latency of the model in milliseconds.
+		"""
+        model = model.to("cpu")
+        
+        model.eval()
+        
+        dummy_input = dummy_input.to("cpu")
+        
+        if self.snn:
+            if isinstance(model, nn.Sequential):
+                for layer_id in range(len(model)):
+                    layer = model[layer_id]
+                    if isinstance((layer), snntorch._neurons.leaky.Leaky):
+                        layer.mem = layer.mem.to("cpu")
+            else:
+                for module in model.modules():
+                    if isinstance((module), snntorch._neurons.leaky.Leaky):
+                        module.mem = module.mem.to("cpu")
+        
+        # warmup
+        for _ in range(n_warmup):
+            _ = model(dummy_input)
+        torch.cuda.synchronize()
+        # real test
+        t1 = time.time()
+        for _ in range(n_test):
+            _ = model(dummy_input)
+            torch.cuda.synchronize()
+        t2 = time.time()
+        return (t2 - t1) / n_test  # average latency in ms
+
+    @torch.no_grad()
+    def evaluate(self, model=None, device=None, Tqdm=True, verbose=False):
+        """
+        Evaluates the model on the test dataset and returns the accuracy.
+
+        Args:
+            verbose (bool): If True, prints the test accuracy.
+
+        Returns:
+            float: The test accuracy as a percentage.
+        """
+        if model != None:
+            self.model = model
+        if device != None:
+            final_device = device
+        else:
+            final_device = self.device
+        
+        self.model.to(final_device)
+        self.model.eval()
+        with torch.no_grad():
+            correct = 0
+            total = 0
+            local_acc = []
+            if Tqdm:
+                loader = tqdm(self.dataloader["test"], desc="test", leave=False)
+            else:
+                loader = self.dataloader["test"]
+            for i, data in enumerate(loader):
+                images, labels = data
+                images, labels = images.to(final_device), labels.to(final_device)
+                # if ( "venum" in self.prune_mode ):
+                #     out = self.model(images)
+                #     total = len(images)
+                #     return
+                if self.snn:
+                    outputs = self.forward_pass_snn(images, mem_out_rec=None)
+                    correct += SF.accuracy_rate(outputs, labels) * outputs.size(1)
+                    total += outputs.size(1)
+                
+                else:
+                    outputs = self.model(images)
+                    _, predicted = torch.max(outputs.data, 1)
+                    total += labels.size(0) - 1
+                    correct += (predicted == labels).sum().item()
+            
+            acc = 100 * correct / total
+            if verbose:
+                print("Test Accuracy: {} %".format(acc))
+            return acc
+          
+    def get_model_size_weights(self, mdl):
+        """
+        Calculates the size of the model's weights in megabytes.
+
+        Args:
+            mdl (torch.nn.Module): The model whose weights size needs to be calculated.
+
+        Returns:
+            float: The size of the model's weights in megabytes.
+        """
+        torch.save(mdl.state_dict(), "tmp.pt")
+        mdl_size = round(os.path.getsize("tmp.pt") / 1e6, 3)
+        os.remove("tmp.pt")
+        return mdl_size
+
+    def get_num_parameters(self, model: nn.Module, count_nonzero_only=False) -> int:
+        """
+        Calculates the total number of parameters in a given PyTorch model.
+
+        :param model (nn.Module): The PyTorch model.
+        :param count_nonzero_only (bool, optional): If True, only counts the number of non-zero parameters.
+                                                    If False, counts all parameters. Defaults to False.
+
+        """
+        
+        num_counted_elements = 0
+        for param in model.parameters():
+            if count_nonzero_only:
+                num_counted_elements += param.count_nonzero()
+            else:
+                num_counted_elements += param.numel()
+        return num_counted_elements
+    
+    def compare_models(self, model_list, model_tags=None):
+        """
+        Compares the performance of two PyTorch models: an original dense model and a pruned and fine-tuned model.
+        Prints a table of metrics including latency, MACs, and model size for both models and their reduction ratios.
+
+        Args:
+        - original_dense_model: a PyTorch model object representing the original dense model
+        - pruned_fine_tuned_model: a PyTorch model object representing the pruned and fine-tuned model
+
+        Returns: None
+        """
+        
+        table_data = {
+            "latency": ["Latency (ms/sample)"],
+            "accuracy": ["Accuracy (%)"],
+            "params": ["Params (M)"],
+            "size": ["Size (MiB)"],
+            "mac": ["MAC (M)"],
+            # "energy": ["Energy (Joules)"],
+        }
+        table = PrettyTable()
+        table.field_names = ["", "Original Model", "Pruned Model", "Quantized Model"]
+        
+        accuracies, latency, params, model_size, macs = [], [], [], [], []
+        skip = 3
+        
+        input_shape = list(next(iter(self.dataloader["test"]))[0].size())
+        input_shape[0] = 1
+        dummy_input = torch.randn(input_shape).to("cpu")
+        file_name_list = ["original_model", "pruned_model", "quantized_model"]
+        if not os.path.exists("weights/"):
+            os.makedirs("weights/")
+        for model, model_file_name in zip(model_list, file_name_list):
+            # print(str(model))
+            # # Parse through snn model and send to cpu
+            if self.snn:
+                if isinstance(model, nn.Sequential):
+                    for layer_id in range(len(model)):
+                        layer = model[layer_id]
+                        if isinstance((layer), snntorch._neurons.leaky.Leaky):
+                            layer.mem = layer.mem.to("cpu")
+                else:
+                    for module in model.modules():
+                        if isinstance((layer), snntorch._neurons.leaky.Leaky):
+                            layer.mem = layer.mem.to("cpu")
+            
+            table_data["accuracy"].append(
+                round(self.evaluate(model=model, device="cpu"), 3)
+            )
+            table_data["latency"].append(
+                round(
+                    self.measure_latency(model=model.to("cpu"), dummy_input=dummy_input)
+                    * 1000,
+                    1,
+                )
+            )
+            table_data["size"].append(self.get_model_size_weights(model))
+            
+            try:
+                model_params = self.get_num_parameters(model, count_nonzero_only=True)
+                if torch.is_tensor(model_params):
+                    model_params = model_params.item()
+                model_params = round(model_params / 1e6, 2)
+                if model_params == 0:
+                    table_data["params"].append("*")
+                else:
+                    table_data["params"].append(model_params)
+            except RuntimeError as e:
+                table_data["params"].append("*")
+            if skip == 1:
+                table_data["mac"].append("*")
+                pass
+            else:
+                try:
+                    mac = self.get_model_macs(model, dummy_input)
+                    table_data["mac"].append(round(mac / 1e6))
+                except AttributeError as e:
+                    table_data["mac"].append("-")
+            
+            ########################################
+            
+            folder_file_name = "weights/" + model_file_name + "/"
+            if not os.path.exists(folder_file_name):
+                os.makedirs(folder_file_name)
+            folder_file_name += model_file_name
+            torch.save(model, folder_file_name + ".pt")
+            torch.save(model.state_dict(), folder_file_name + "_weights.pt")
+            traced_model = torch.jit.trace(model, dummy_input)
+            torch.jit.save(torch.jit.script(traced_model), folder_file_name + ".pt")
+            
+            ########################################
+            # Save model with pt,.pth and jit
+            skip -= 1
+        
+        for key, value in table_data.items():
+            table.add_row(value)
+        print(
+            "\n \n============================== Comparison Table =============================="
+        )
+        print(table)
--- sconce/sconce.py
+++ sconce/sconce.py
@@ -39,6 +39,8 @@ import ipdb
 import snntorch
 from snntorch import functional as SF
 
+from preformance_eval import PerformanceEval
+
 import warnings
 
 # Suppress all warnings
@@ -154,8 +156,15 @@ class sconce:
 		
 		self.bitwidth = 4
 		
+		self.preformance_eval = PerformanceEval(
+			dataloader=self.dataloader,
+			snn=self.snn,
+			model=self.model,
+			snn_num_steps=self.snn_num_steps
+		)
+
 		self.device = None
-	
+
 	def forward_pass_snn(self, data, mem_out_rec=None):
 		"""
 		Perform a forward pass through the spiking neural network (SNN).
@@ -182,7 +191,7 @@ class sconce:
 			return torch.stack(spk_rec), torch.stack(mem_rec)
 		else:
 			return torch.stack(spk_rec)
-	
+
 	def train(self, model=None) -> None:
 		"""
 		Trains the model for a specified number of epochs using the specified dataloader and optimizer.
@@ -239,7 +248,7 @@ class sconce:
 			
 			running_loss = 0.0
 			
-			validation_acc = self.evaluate()
+			validation_acc = self.preformance_eval.evaluate()
 			if validation_acc > val_acc:
 				print(
 					f"Epoch:{epoch + 1} Train Loss: {running_loss / 2000:.5f} Validation Accuracy: {validation_acc:.5f}"
@@ -278,57 +287,6 @@ class sconce:
 			
 			return
 	
-	@torch.no_grad()
-	def evaluate(self, model=None, device=None, Tqdm=True, verbose=False):
-		"""
-		Evaluates the model on the test dataset and returns the accuracy.
-
-		Args:
-		  verbose (bool): If True, prints the test accuracy.
-
-		Returns:
-		  float: The test accuracy as a percentage.
-		"""
-		if model != None:
-			self.model = model
-		if device != None:
-			final_device = device
-		else:
-			final_device = self.device
-		
-		self.model.to(final_device)
-		self.model.eval()
-		with torch.no_grad():
-			correct = 0
-			total = 0
-			local_acc = []
-			if Tqdm:
-				loader = tqdm(self.dataloader["test"], desc="test", leave=False)
-			else:
-				loader = self.dataloader["test"]
-			for i, data in enumerate(loader):
-				images, labels = data
-				images, labels = images.to(final_device), labels.to(final_device)
-				# if ( "venum" in self.prune_mode ):
-				#     out = self.model(images)
-				#     total = len(images)
-				#     return
-				if self.snn:
-					outputs = self.forward_pass_snn(images, mem_out_rec=None)
-					correct += SF.accuracy_rate(outputs, labels) * outputs.size(1)
-					total += outputs.size(1)
-				
-				else:
-					outputs = self.model(images)
-					_, predicted = torch.max(outputs.data, 1)
-					total += labels.size(0) - 1
-					correct += (predicted == labels).sum().item()
-			
-			acc = 100 * correct / total
-			if verbose:
-				print("Test Accuracy: {} %".format(acc))
-			return acc
-	
 	########## Model Profiling ##########
 	def get_model_macs(self, model, inputs) -> int:
 		"""
@@ -343,41 +301,12 @@ class sconce:
 		"""
 		return profile_macs(model, inputs)
 	
-	def measure_inference_latency(
-			self, model, device, input_data, num_samples=100, num_warmups=10
-	):
-		model.to(device)
-		model.eval()
-		
-		x = input_data.to(device)
-		
-		with torch.no_grad():
-			for _ in range(num_warmups):
-				_ = model(x)
-		torch.cuda.synchronize()
-		
-		with torch.no_grad():
-			start_time = time.time()
-			for _ in range(num_samples):
-				_ = model(x)
-				torch.cuda.synchronize()
-			end_time = time.time()
-		elapsed_time = end_time - start_time
-		elapsed_time_ave = elapsed_time / num_samples
-		
-		return elapsed_time_ave
-	
 	def save_torchscript_model(self, model, model_dir, model_filename):
 		if not os.path.exists(model_dir):
 			os.makedirs(model_dir)
 		model_filepath = os.path.join(model_dir, model_filename)
 		
 		torch.jit.save(torch.jit.script(model), model_filepath)
-	
-	def load_torchscript_model(self, model_filepath, device):
-		model = torch.jit.load(model_filepath, map_location=device)
-		
-		return model
 	
 	def get_sparsity(self, tensor: torch.Tensor) -> float:
 		"""
@@ -411,39 +340,6 @@ class sconce:
 			num_elements += param.numel()
 		return 1 - float(num_nonzeros) / num_elements
 	
-	def get_model_size_weights(self, mdl):
-		"""
-		Calculates the size of the model's weights in megabytes.
-
-		Args:
-			mdl (torch.nn.Module): The model whose weights size needs to be calculated.
-
-		Returns:
-			float: The size of the model's weights in megabytes.
-		"""
-		torch.save(mdl.state_dict(), "tmp.pt")
-		mdl_size = round(os.path.getsize("tmp.pt") / 1e6, 3)
-		os.remove("tmp.pt")
-		return mdl_size
-	
-	def get_num_parameters(self, model: nn.Module, count_nonzero_only=False) -> int:
-		"""
-		Calculates the total number of parameters in a given PyTorch model.
-
-		:param model (nn.Module): The PyTorch model.
-		:param count_nonzero_only (bool, optional): If True, only counts the number of non-zero parameters.
-													If False, counts all parameters. Defaults to False.
-
-		"""
-		
-		num_counted_elements = 0
-		for param in model.parameters():
-			if count_nonzero_only:
-				num_counted_elements += param.count_nonzero()
-			else:
-				num_counted_elements += param.numel()
-		return num_counted_elements
-	
 	def get_model_size(
 			self, model: nn.Module, data_width=32, count_nonzero_only=False
 	) -> int:
@@ -452,50 +348,7 @@ class sconce:
 		:param data_width: #bits per element
 		:param count_nonzero_only: only count nonzero weights
 		"""
-		return self.get_num_parameters(model, count_nonzero_only) * data_width
-	
-	@torch.no_grad()
-	def measure_latency(self, model, dummy_input, n_warmup=20, n_test=100):
-		"""
-		Measures the average latency of a given PyTorch model by running it on a dummy input multiple times.
-
-		Args:
-		  model (nn.Module): The PyTorch model to measure the latency of.
-		  dummy_input (torch.Tensor): A dummy input to the model.
-		  n_warmup (int, optional): The number of warmup iterations to run before measuring the latency. Defaults to 20.
-		  n_test (int, optional): The number of iterations to run to measure the latency. Defaults to 100.
-
-		Returns:
-		  float: The average latency of the model in milliseconds.
-		"""
-		model = model.to("cpu")
-		
-		model.eval()
-		
-		dummy_input = dummy_input.to("cpu")
-		
-		if self.snn:
-			if isinstance(model, nn.Sequential):
-				for layer_id in range(len(model)):
-					layer = model[layer_id]
-					if isinstance((layer), snntorch._neurons.leaky.Leaky):
-						layer.mem = layer.mem.to("cpu")
-			else:
-				for module in model.modules():
-					if isinstance((module), snntorch._neurons.leaky.Leaky):
-						module.mem = module.mem.to("cpu")
-		
-		# warmup
-		for _ in range(n_warmup):
-			_ = model(dummy_input)
-		torch.cuda.synchronize()
-		# real test
-		t1 = time.time()
-		for _ in range(n_test):
-			_ = model(dummy_input)
-			torch.cuda.synchronize()
-		t2 = time.time()
-		return (t2 - t1) / n_test  # average latency in ms
+		return self.preformance_eval.get_num_parameters(model, count_nonzero_only) * data_width
 	
 	def plot_weight_distribution(self, bins=256, count_nonzero_only=False):
 		"""
@@ -651,7 +504,7 @@ class sconce:
 				if hit_flag == True:
 					# if self.prune_mode == "venum_sensitivity":
 					#     self.prune_mode = original_prune_mode
-					acc = self.evaluate(Tqdm=False) - dense_model_accuracy
+					acc = self.preformance_eval.evaluate(Tqdm=False) - dense_model_accuracy
 					# if ("venum" in self.prune_mode):
 					#     self.prune_mode = "venum_sensitivity"
 					self.model = copy.deepcopy(original_model)
@@ -927,7 +780,7 @@ class sconce:
 			model=self.model, count_nonzero_only=True
 		)
 		print(f"\nOriginal Dense Model Size Model={dense_model_size / MiB:.2f} MiB")
-		dense_validation_acc = self.evaluate(verbose=False)
+		dense_validation_acc = self.preformance_eval.evaluate(verbose=False)
 		print("Original Model Validation Accuracy:", dense_validation_acc, "%")
 		self.dense_model_valid_acc = dense_validation_acc
 		
@@ -1023,12 +876,12 @@ class sconce:
 		pruned_model_size = self.get_model_size(
 			model=pruned_model, count_nonzero_only=True
 		)
-		pruned_validation_acc = self.evaluate(verbose=False)
+		pruned_validation_acc = self.preformance_eval.evaluate(verbose=False)
 		
 		print(
 			f"\nPruned Model has size={pruned_model_size / MiB:.2f} MiB(non-zeros) = {pruned_model_size / dense_model_size * 100:.2f}% of Original model size"
 		)
-		pruned_model_acc = self.evaluate()
+		pruned_model_acc = self.preformance_eval.evaluate()
 		print(
 			f"\nPruned Model has Accuracy={pruned_model_acc :.2f} % = {pruned_model_acc - dense_validation_acc :.2f}% of Original model Accuracy"
 		)
@@ -1056,7 +909,7 @@ class sconce:
 		fine_tuned_pruned_model_size = self.get_model_size(
 			model=pruned_model, count_nonzero_only=True
 		)
-		fine_tuned_validation_acc = self.evaluate(verbose=False)
+		fine_tuned_validation_acc = self.preformance_eval.evaluate(verbose=False)
 		
 		if verbose:
 			print(
@@ -1071,7 +924,7 @@ class sconce:
 		
 		model_list = [original_dense_model, pruned_model, quantized_model]
 		
-		self.compare_models(model_list=model_list)
+		self.preformance_eval.compare_models(model_list=model_list)
 	
 	def evaluate_model(self, model, test_loader, device, criterion=None):
 		model.eval()
@@ -1293,107 +1146,6 @@ class sconce:
 		return model_int8, model_fp32_trained
 	
 	# def compare_models(self, original_dense_model, pruned_fine_tuned_model, quantization=False,accuracies=None):
-	
-	def compare_models(self, model_list, model_tags=None):
-		"""
-		Compares the performance of two PyTorch models: an original dense model and a pruned and fine-tuned model.
-		Prints a table of metrics including latency, MACs, and model size for both models and their reduction ratios.
-
-		Args:
-		- original_dense_model: a PyTorch model object representing the original dense model
-		- pruned_fine_tuned_model: a PyTorch model object representing the pruned and fine-tuned model
-
-		Returns: None
-		"""
-		
-		table_data = {
-			"latency": ["Latency (ms/sample)"],
-			"accuracy": ["Accuracy (%)"],
-			"params": ["Params (M)"],
-			"size": ["Size (MiB)"],
-			"mac": ["MAC (M)"],
-			# "energy": ["Energy (Joules)"],
-		}
-		table = PrettyTable()
-		table.field_names = ["", "Original Model", "Pruned Model", "Quantized Model"]
-		
-		accuracies, latency, params, model_size, macs = [], [], [], [], []
-		skip = 3
-		
-		input_shape = list(next(iter(self.dataloader["test"]))[0].size())
-		input_shape[0] = 1
-		dummy_input = torch.randn(input_shape).to("cpu")
-		file_name_list = ["original_model", "pruned_model", "quantized_model"]
-		if not os.path.exists("weights/"):
-			os.makedirs("weights/")
-		for model, model_file_name in zip(model_list, file_name_list):
-			# print(str(model))
-			# # Parse through snn model and send to cpu
-			if self.snn:
-				if isinstance(model, nn.Sequential):
-					for layer_id in range(len(model)):
-						layer = model[layer_id]
-						if isinstance((layer), snntorch._neurons.leaky.Leaky):
-							layer.mem = layer.mem.to("cpu")
-				else:
-					for module in model.modules():
-						if isinstance((layer), snntorch._neurons.leaky.Leaky):
-							layer.mem = layer.mem.to("cpu")
-			
-			table_data["accuracy"].append(
-				round(self.evaluate(model=model, device="cpu"), 3)
-			)
-			table_data["latency"].append(
-				round(
-					self.measure_latency(model=model.to("cpu"), dummy_input=dummy_input)
-					* 1000,
-					1,
-				)
-			)
-			table_data["size"].append(self.get_model_size_weights(model))
-			
-			try:
-				model_params = self.get_num_parameters(model, count_nonzero_only=True)
-				if torch.is_tensor(model_params):
-					model_params = model_params.item()
-				model_params = round(model_params / 1e6, 2)
-				if model_params == 0:
-					table_data["params"].append("*")
-				else:
-					table_data["params"].append(model_params)
-			except RuntimeError as e:
-				table_data["params"].append("*")
-			if skip == 1:
-				table_data["mac"].append("*")
-				pass
-			else:
-				try:
-					mac = self.get_model_macs(model, dummy_input)
-					table_data["mac"].append(round(mac / 1e6))
-				except AttributeError as e:
-					table_data["mac"].append("-")
-			
-			########################################
-			
-			folder_file_name = "weights/" + model_file_name + "/"
-			if not os.path.exists(folder_file_name):
-				os.makedirs(folder_file_name)
-			folder_file_name += model_file_name
-			torch.save(model, folder_file_name + ".pt")
-			torch.save(model.state_dict(), folder_file_name + "_weights.pt")
-			traced_model = torch.jit.trace(model, dummy_input)
-			torch.jit.save(torch.jit.script(traced_model), folder_file_name + ".pt")
-			
-			########################################
-			# Save model with pt,.pth and jit
-			skip -= 1
-		
-		for key, value in table_data.items():
-			table.add_row(value)
-		print(
-			"\n \n============================== Comparison Table =============================="
-		)
-		print(table)
 	
 	# # accuracies = accuracies
 	# if( accuracies == None):
