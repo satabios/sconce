9e8bb035e373380506bf1bca9b1c4977b86aecde
satabios <max.satabiossathya@gmail.com>
2025-01-26 16:53:05 -0800

    updated model_analyzer

--- sconce/__init__.py
+++ sconce/__init__.py
@@ -2,6 +2,5 @@ from .sconce import sconce
 from .perf import performance
 from .quanter import quantization
 from .pruner import prune
-from .model_analyzer import model_analyzer
 
-__all__ = ["sconce", "performance", "quantization", "prune", "model_analyzer"]
+__all__ = ["sconce", "performance", "quantization", "prune"]
