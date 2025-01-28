47958c90aadc45bb9d78569ce3af9dce031c00f6
satabios <max.satabiossathya@gmail.com>
2025-01-28 09:26:25 -0800

    added file_size.sh

---
+++ scripts/file_size.sh
@@ -0,0 +1,8 @@
+#! /bin/bash
+
+git rev-list --objects --all | \
+  git cat-file --batch-check='%(objecttype) %(objectname) %(objectsize) %(rest)' | \
+  sed -n 's/^blob //p' | \
+  awk '{size=$2/1024/1024; printf "%s %.2f MB %s\n", $1, size, $3}' | \
+  sort -k2nr | \
+  head -10
